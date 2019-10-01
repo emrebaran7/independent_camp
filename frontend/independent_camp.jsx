fbf5a53aa8e8c8244382b4c84324ae1165a1907b
Emre Ersolmaz <emre.ersolmaz@gmail.com>
2019-10-01 17:59:51 -0400

    Comment out store in entry file

--- frontend/independent_camp.jsx
+++ frontend/independent_camp.jsx
@@ -2,11 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
-import Root from './components/root';
-import configureStore from './store/store';
+// import Root from './components/root';
+// import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    let store = configureStore();
+    // let store = configureStore();
     const root = document.getElementbyId('root');
     ReactDOM.render(<h1>Welcome to Independent Camp</h1>, root)
 })
