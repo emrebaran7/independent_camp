7b88ee3b7d09b644aa812407eea8fcf07795613d
Emre Ersolmaz <emre.ersolmaz@gmail.com>
2019-10-08 17:58:22 -0400

    Change in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,8 +33,11 @@ class User < ApplicationRecord
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
-        return nil unless user && user.is_password?(password)
-        user
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
     end
 
     def password=(password) 
