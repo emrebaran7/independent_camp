6d0573b3fd184359e4171708d16542a414ef0647
Emre Ersolmaz <emre.ersolmaz@gmail.com>
2019-10-10 09:19:27 -0400

    album css work

--- frontend/components/album_show/album_show.jsx
+++ frontend/components/album_show/album_show.jsx
@@ -29,15 +29,18 @@ class AlbumShow extends React.Component {
         //     </li>
         // })
         
+        // const tracks = this.props.album.tracks
+        
 
         return (
             <div className='album-show-container'>
                 <div className='album-show-left'>
-                    <header>{this.props.album.album.title}</header>
+                    <header>{this.props.album.album.title} <br/>
+                    by </header>
                     {/* audio player  */}
                     <strong>Digital Album</strong>
                     <p>High-quality download in MP3</p>
-                    {/* <ul className='tracks'>{tracks}</ul> */}
+                    {/* <ul className='tracks'>{this.props.album.tracks}</ul> */}
                     <p className='album-description'>{this.props.album.album.description}</p>
                     <p className='album-release-date'>{this.props.album.album.release_date}</p>
                     <p className='album-release-credits'>{this.props.album.album.credits}</p>
